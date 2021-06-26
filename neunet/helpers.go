615215d5646d05cdc656fe02115a5f4c5655752e
Arturo Bermejo <1109287+arturobermejo@users.noreply.github.com>
2021-06-26 09:31:28 -0500

    show accuracy

--- neunet/helpers.go
+++ neunet/helpers.go
@@ -5,6 +5,7 @@ import (
 	"math"
 
 	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat"
 	"gonum.org/v1/gonum/stat/distuv"
 )
 
@@ -90,4 +91,39 @@ func matrixAdd(m, n mat.Matrix) *mat.Dense {
 func matrixPrint(X mat.Matrix) {
 	fa := mat.Formatted(X, mat.Prefix(""), mat.Squeeze())
 	fmt.Printf("%v\n", fa)
+}
+
+func accuracy(m, n *mat.Dense) float64 {
+	r, _ := m.Dims()
+	result := mat.NewDense(1, r, nil)
+
+	for i := 0; i < r; i++ {
+		mi := argmax(m.RawRowView(i))
+		ni := argmax(n.RawRowView(i))
+
+		if mi == ni {
+			result.Set(0, mi, 1)
+		}
+	}
+
+	return stat.Mean(result.RawMatrix().Data, nil)
+}
+
+func argmax(s []float64) int {
+	var maxIdx int
+
+	n := len(s)
+	max := s[0]
+	maxIdx = 0
+
+	for i := 1; i < n; i++ {
+		v := s[i]
+		if v > max {
+			max = v
+			maxIdx = i
+			break
+		}
+	}
+
+	return maxIdx
 }
--- neunet/train.go
+++ neunet/train.go
@@ -9,7 +9,7 @@ func Train() {
 	inputData, outputData := dl.FromFile("data/messages", "data/categories")
 
 	voca := NewVocabulary(inputData, outputData)
-	model := NewModel(voca.GetInputSize(), 4, voca.GetOutputSize())
+	model := NewModel(voca.GetInputSize(), 10, voca.GetOutputSize())
 	learningRate := 0.000001
 	epochs := 100
 
@@ -24,11 +24,14 @@ func Train() {
 		outputPred := model.Forward(input)
 		loss := criterion.Forward(outputPred, output)
 
+		accuracy := accuracy(outputPred, output)
+
+		// Backward Propagation
+
 		// Derivative of loss function respect to softmax input, that means:
 		// dinputs => dloss/dz = dloss/dsoftmax * dsoftmax/dz
 		dinputs := matrixSubtract(outputPred, output)
 
-		// output layer
 		model.outputLayer.Backward(dinputs)
 		model.reluLayer.Backward(model.outputLayer.dinputs)
 		model.hiddenLayer.Backward(model.reluLayer.dinputs)
@@ -37,7 +40,7 @@ func Train() {
 		optimizer.UpdateParameters(model.hiddenLayer)
 
 		if epoch%10 == 0 {
-			fmt.Printf("epoch: %v, loss: %v\n", epoch, loss)
+			fmt.Printf("epoch: %v, acc: %v, loss: %v\n", epoch, accuracy, loss)
 		}
 	}
 
