f45319cff7a56c78766e427a3a9056e2b9d2f6a1
Arturo Bermejo <1109287+arturobermejo@users.noreply.github.com>
2021-06-29 19:19:59 -0500

    add relu activation test

--- neunet/activations_test.go
+++ neunet/activations_test.go
@@ -22,3 +22,25 @@ func TestSoftmaxActivation(t *testing.T) {
 
 	assert.True(t, mat.Equal(a.output, res))
 }
+
+func TestReluActivation(t *testing.T) {
+	a := NewReLUActivation()
+
+	// Forward
+	inputs := mat.NewDense(2, 3, []float64{-1.0, 0.0, 1.0, -0.5, 0.0, 0.5})
+	a.Forward(inputs)
+
+	assert.True(t, mat.Equal(a.inputs, inputs))
+
+	res := mat.NewDense(2, 3, []float64{0.0, 0.0, 1.0, 0.0, 0.0, 0.5})
+
+	assert.True(t, mat.Equal(a.output, res))
+
+	// Backward
+	inputs = mat.NewDense(2, 3, []float64{1.0, 2.0, 3.0, -0.1, -0.2, -0.3})
+	a.Backward(inputs)
+
+	res = mat.NewDense(2, 3, []float64{0.0, 0.0, 3.0, 0.0, 0.0, -0.3})
+
+	assert.True(t, mat.Equal(a.dinputs, res))
+}
