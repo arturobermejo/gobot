60460860828ea8e9e4609b4920c947a2509c2b0a
Arturo Bermejo <1109287+arturobermejo@users.noreply.github.com>
2021-06-30 09:54:11 -0500

    add tests dataloader helpers

--- neunet/dataloader_test.go
+++ neunet/dataloader_test.go
@@ -4,9 +4,28 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gonum.org/v1/gonum/mat"
 )
 
-func TestDataLoader(t *testing.T) {
-	// dl := NewDataLoader()
-	assert.True(t, true, "True is true!")
+func TestGetVocab(t *testing.T) {
+	vocab := GetVocab([]string{"one two", "three four"})
+	expected := map[string]int{"one": 0, "two": 1, "three": 2, "four": 3}
+
+	assert.Equal(t, expected, vocab)
+}
+
+func TestOneHotEncode(t *testing.T) {
+	vocab := map[string]int{"one": 0, "two": 1, "three": 2, "four": 3}
+	r := OneHotEncode([]string{"two one one", "one any four"}, vocab)
+	expected := mat.NewDense(2, 4, []float64{1, 1, 0, 0, 1, 0, 0, 1})
+
+	assert.Equal(t, expected, r)
+}
+
+func TestOutputDecode(t *testing.T) {
+	vocab := map[string]int{"cat1": 0, "cat2": 1, "cat3": 2}
+	s, p := OutputDecode(mat.NewDense(1, 3, []float64{0.5, 0.6, 0.2}), vocab)
+
+	assert.Equal(t, "cat2", s)
+	assert.Equal(t, 0.6, p)
 }
